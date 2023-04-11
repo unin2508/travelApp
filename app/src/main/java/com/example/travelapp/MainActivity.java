2c875fe322051d9431ca8f5849f81e8d96e31b25
Linh Nguyen <unin2508@gmail.com>
2023-04-11 11:03:04 +0700

    first commit

---
+++ app/src/main/java/com/example/travelapp/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.travelapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navigationView = findViewById(R.id.bottom_nav);
+        replaceFragment(new HomeFragment());
+        navigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        replaceFragment(new HomeFragment());
+                        break;
+                    case R.id.cart:
+                        replaceFragment(new CartFragment());
+                        break;
+
+                    case R.id.saved:
+                        replaceFragment(new SavedFragment());
+                        break;
+
+                    case R.id.user:
+                        replaceFragment(new UserFragment());
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layput,fragment);
+        fragmentTransaction.commit();
+    }
+}
