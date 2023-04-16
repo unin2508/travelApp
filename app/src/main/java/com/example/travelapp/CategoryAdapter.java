65bbd4ba8d18f0cb968db679b1db988f70233c7a
Linh Nguyen <unin2508@gmail.com>
2023-04-17 01:37:36 +0700

    add Category

---
+++ app/src/main/java/com/example/travelapp/CategoryAdapter.java
@@ -0,0 +1,111 @@
+package com.example.travelapp;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.CategortViewHolder> {
+    private Context context;
+    private List<Category> categoryList;
+    private int selectedIndex = -1;
+
+    public CategoryAdapter(Context context){
+        this.context = context;
+    }
+
+    public void setData(List<Category> categoryList){
+        this.categoryList = categoryList;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public CategortViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_category,parent,false);
+        return new CategortViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CategortViewHolder holder, int position) {
+        Category category = this.categoryList.get(position);
+        if (category == null){
+            return;
+        }
+        holder.nameTV.setText(category.getName());
+        switch (category.getName()){
+            case "Places":
+                holder.imageCategory.setImageResource(R.drawable.place);
+                break;
+            case "Flights":
+                holder.imageCategory.setImageResource(R.drawable.plane);
+                break;
+            case "Trains":
+                holder.imageCategory.setImageResource(R.drawable.train);
+                break;
+            case "Buses":
+                holder.imageCategory.setImageResource(R.drawable.bus_stop);
+                break;
+            case "Taxi":
+                holder.imageCategory.setImageResource(R.drawable.taxi);
+                break;
+            default:
+                break;
+        }
+        holder.categoryCV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedIndex = position;
+                notifyDataSetChanged();
+                Log.d("debug","test");
+
+
+            }
+        });
+
+        if (selectedIndex == position){
+            holder.nameTV.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
+            holder.nameTV.setTextSize(14);
+            holder.nameTV.setTextColor(0xff000000);
+        } else {
+            holder.nameTV.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
+            holder.nameTV.setTextSize(12);
+            holder.nameTV.setTextColor(0xff848484);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        if (this.categoryList != null){
+            return this.categoryList.size();
+        }
+        return 0;
+    }
+
+    public class CategortViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView imageCategory;
+        private TextView nameTV;
+        private CardView categoryCV;
+
+        public CategortViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            nameTV = itemView.findViewById(R.id.name_category);
+            imageCategory = itemView.findViewById(R.id.image_category);
+            categoryCV = itemView.findViewById(R.id.cardview_category);
+        }
+    }
+}
--- app/src/main/java/com/example/travelapp/fragment/HomeFragment.java
+++ app/src/main/java/com/example/travelapp/fragment/HomeFragment.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -13,8 +15,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 
+import com.example.travelapp.Category;
+import com.example.travelapp.CategoryAdapter;
 import com.example.travelapp.R;
 import com.example.travelapp.activity.LoginActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,7 +29,9 @@ import com.example.travelapp.activity.LoginActivity;
  * create an instance of this fragment.
  */
 public class HomeFragment extends Fragment {
-
+    ImageButton imageButton;
+    RecyclerView recyclerView;
+    CategoryAdapter categoryAdapter;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -50,7 +59,7 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        ImageButton imageButton = view.findViewById(R.id.filter_btn);
+        imageButton = view.findViewById(R.id.filter_btn);
         if (imageButton != null) {
             imageButton.setOnClickListener(new ViewGroup.OnClickListener() {
                 @Override
@@ -62,5 +71,22 @@ public class HomeFragment extends Fragment {
                 }
             });
         }
+        recyclerView = view.findViewById(R.id.category_rv);
+        categoryAdapter = new CategoryAdapter(view.getContext());
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(view.getContext(),RecyclerView.HORIZONTAL,false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        categoryAdapter.setData(getListCategory());
+        recyclerView.setAdapter(categoryAdapter);
+    }
+
+    private List<Category> getListCategory() {
+        List<Category> list = new ArrayList<>();
+        list.add(new Category(0,"Places",null));
+        list.add(new Category(1,"Flights",null));
+        list.add(new Category(2,"Trains",null));
+        list.add(new Category(3,"Buses",null));
+        list.add(new Category(4,"Taxi",null));
+        return list;
     }
 }
